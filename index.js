ecb589479a551dc673f969e61136d73265f0a867
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-05-10 07:54:30 +0100

    updated file

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous", true);
+xhr.setRequestHeader("X-Mashape-Key", "yA0hUy71G0msh9PZ7bFyQ9Q46rwgp1YgetAjsnQTQXv5OkJjtb", false);
+xhr.setRequestHeader("Accept", "application/json", false);
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded", false);
+xhr.onload = function (event) {
+  	if (xhr.readyState === 4 && xhr.status === 200){
+  		let response = JSON.parse(xhr.responseText);
+      	console.log("Quote: " + response.quote);
+      	console.log("Quote Author: " + response.author);
+  	} else {
+      	console.error(xhr.statusText);
+  	}
+};
+
+xhr.onerror = function (event) {
+  console.error(xhr.statusText);
+};
+xhr.send(null);
