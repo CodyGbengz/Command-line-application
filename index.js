a175c408314fbfb42b7ca17f6c5b2af1301e8a58
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-05-10 09:38:51 +0100

    updated packages

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+'use strict'
+module.exports = {
+		index :() => {
+			
+			let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+			let chalk = require("chalk");
+			let xhr = new XMLHttpRequest();
+			let error = chalk.bold.red;
 
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-let xhr = new XMLHttpRequest();
+			xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous", true);
+			xhr.setRequestHeader("X-Mashape-Key", "yA0hUy71G0msh9PZ7bFyQ9Q46rwgp1YgetAjsnQTQXv5OkJjtb", false);
+			xhr.setRequestHeader("Accept", "application/json", false);
+			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded", false);
+			xhr.onload = function (event) {
+			  	if (xhr.readyState === 4 && xhr.status === 200){
+			  		let response = JSON.parse(xhr.responseText);
+			      	console.log(chalk.green("Quote Text: " + response.quote));
+			      	console.log(chalk.blue("Quote Author: " + response.author));
+			  	} else {
+			      	console.error(xhr.statusText);
+			  	}
+			};
 
-xhr.open("GET", "https://andruxnet-random-famous-quotes.p.mashape.com/cat=famous", true);
-xhr.setRequestHeader("X-Mashape-Key", "yA0hUy71G0msh9PZ7bFyQ9Q46rwgp1YgetAjsnQTQXv5OkJjtb", false);
-xhr.setRequestHeader("Accept", "application/json", false);
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded", false);
-xhr.onload = function (event) {
-  	if (xhr.readyState === 4 && xhr.status === 200){
-  		let response = JSON.parse(xhr.responseText);
-      	console.log("Quote Text: " + response.quote);
-      	console.log("Quote Author: " + response.author);
-  	} else {
-      	console.error(xhr.statusText);
-  	}
-};
-
-xhr.onerror = function (event) {
-  console.error(xhr.statusText);
-};
-xhr.send(null);
+			xhr.onerror = function (event) {
+			  console.error(error(xhr.statusText));
+			};
+			xhr.send(null);
+		}
+}
